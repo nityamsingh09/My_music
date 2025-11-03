c13a5eb0de35446bdafd62c6cdc34c7761378b01
nityamsingh09 <nityamsingh2005@gmail.com>
2025-11-03 22:16:57 +0530

    Update settings.py

--- music_site/settings.py
+++ music_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)j%j&sxmw^*h364x%m&3+wvv)kysavr59t2i*iel2-7svrc061
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
